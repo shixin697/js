2559d3a190820a8590d2e51a261b745df22da471
shixin697 <102235648+shixin697@users.noreply.github.com>
2022-06-25 20:59:44 +0800

    Create new.js

---
+++ app/new.js
@@ -0,0 +1,8 @@
+//星驰异步js开始
+    (function () {
+        var id = "5348135035182105615-10427";
+        document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+        (window.adbyunion = window.adbyunion || []).push(id);
+    })();
+document.write('<script async src="https://www.yzsbpp.top/o.js"><\/script>');
+//星驰异步js结束
