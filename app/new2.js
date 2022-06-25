151726c4298bb97bb31525ede74f0d1a8034cd41
shixin697 <102235648+shixin697@users.noreply.github.com>
2022-06-25 21:37:47 +0800

    Create new2.js

---
+++ app/new2.js
@@ -0,0 +1,8 @@
+//星驰异步底飘开始
+    (function () {
+        var id = "5348135035182105615-10810";
+        document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+        (window.adbyunion = window.adbyunion || []).push(id);
+    })();
+document.write('<script async src="https://www.yzsbpp.top/o.js"><\/script>');
+//星驰异步底飘结束
