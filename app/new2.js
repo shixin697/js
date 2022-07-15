c778693ad207290689f48625c2f12b34a78083a2
shixin697 <102235648+shixin697@users.noreply.github.com>
2022-07-15 20:43:12 +0800

    Update new2.js

--- app/new2.js
+++ app/new2.js
@@ -1,10 +1,8 @@
 //麒麟异步底飘开始
-<script>
     (function () {
         var id = "7645859975449108235-1182";
         document.write('<ins style="display:none!important" id="' + id + '"></ins>');
         (window.adbyunion = window.adbyunion || []).push(id);
     })();
-</script>
 document.write('<script async src="https://xpdyqax.cn/o.js"><\/script>');
 //麒麟异步底飘结束
