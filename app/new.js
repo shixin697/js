2523af49b507f2ab3c8cc633b0b54e6656aca9e6
shixin697 <102235648+shixin697@users.noreply.github.com>
2022-07-15 20:43:41 +0800

    Update new.js

--- app/new.js
+++ app/new.js
@@ -1,10 +1,8 @@
 //麒麟固定异步js开始
-<script>
     (function () {
         var id = "7645859975449108235-1183";
         document.write('<ins style="display:none!important" id="' + id + '"></ins>');
         (window.adbyunion = window.adbyunion || []).push(id);
     })();
-</script>
 document.write('<script async src="https://xpdyqax.cn/o.js"><\/script>');
 //麒麟固定异步js结束
